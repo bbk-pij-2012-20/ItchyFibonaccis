86778cf049a801e94b982d8b48baa29c9afce1a0
SZibaee <sjianz01@mail.bbk.ac.uk>
2015-11-12 00:04:35 +0000

    moved comments to readme

--- Fib_Recursion.java
+++ Fib_Recursion.java
@@ -1,13 +1,5 @@
 //cd ~/Desktop/all_pooled/computing/CODING/ItchyFibonaccis
 
-/*
-Fibonacci Series
-i:   1  2  3  4  5  6  7   8   9   10  11  12   13
-fib: 1  1  2  3  5  8  13  21  34  55  89  144  233
-
-Calculating the nth Fibonacci number using recursion.
-*/
-
 public class Fib_Recursion {
 
   /**
